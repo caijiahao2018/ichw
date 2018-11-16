bcb1cc7f077b21e0302befe80d372d3c0d9e593b
caijiahao2018 <43374703+caijiahao2018@users.noreply.github.com>
2018-11-16 14:36:41 +0800

    Add files via upload

---
+++ planets.py
@@ -0,0 +1,13 @@
+import turtle
+wn = turtle.Screen()
+sun = turtle.Turtle()
+sun.shape("circle")
+sun.fillcolor("red")
+
+Mercury = turtle.Turtle()
+turtle.penup()
+turtle.goto(100,0)
+turtle.pendown()
+Mercury = turtle.Turtle()
+Mercury.shape("circle")
+Mercury.fillcolor("blue")
